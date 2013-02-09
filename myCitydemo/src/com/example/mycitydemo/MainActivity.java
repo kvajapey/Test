225a703587a636d26aacd0803783bb5c9b12d99c
Kiran Vajapey <kvajapey@ucsd.edu>
2013-02-08 18:40:48 -0800

    eclipse to github test

--- myCitydemo/src/com/example/mycitydemo/MainActivity.java
+++ myCitydemo/src/com/example/mycitydemo/MainActivity.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.mycitydemo;
 
 import java.util.ArrayList;
@@ -49,3 +50,57 @@ public class MainActivity extends Activity {
     }
 }
 
+=======
+package com.example.mycitydemo;
+
+//i am adding this test comment for github
+
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    EditText un,pw;
+	TextView error;
+    Button ok;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        un=(EditText)findViewById(R.id.et_un);
+        pw=(EditText)findViewById(R.id.et_pw);
+        ok=(Button)findViewById(R.id.btn_login);
+        error=(TextView)findViewById(R.id.tv_error);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+            	ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+            	postParameters.add(new BasicNameValuePair("username", un.getText().toString()));
+            	postParameters.add(new BasicNameValuePair("password", pw.getText().toString()));
+
+            	String response = null;
+            	try {
+            	    
+            	} catch (Exception e) {
+            		un.setText(e.toString());
+            	}
+
+            }
+        });
+    }
+}
+
+>>>>>>> eclipse to github test
